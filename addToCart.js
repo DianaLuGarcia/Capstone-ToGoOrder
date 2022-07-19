11d8cd6a26ed1b23b6cae55eadebdeb8172f9c89
Diana Lupita Garcia <75147104+DianaLuGarcia@users.noreply.github.com>
2022-07-18 22:53:53 -0600

    Added new input fields in checkout

--- addToCart.js
+++ addToCart.js
@@ -1,44 +1,44 @@
-
-
 let addItemId = 0;
-let cartMath = document.querySelector('.cart-math')
+let cartMath = document.querySelector(".cart-math");
 let cart = [];
 
-function addToCart (item){
-cart.push(item);
-disCart();
-disPrice();
+//**************Adds Item + Price to cart **************************
+function addToCart(item) {
+  cart.push(item);
+  disCart();
+  disPrice();
 }
 
-function disCart (){
-let cartItems = document.getElementById('title');
-//clears items in cart when adding new (to avoid duplicates)
-cartItems.innerHTML = "";
+function disCart(id) {
+  let cartItems = document.getElementById("title");
+  //clears items in cart when adding new (to avoid duplicates)
+  cartItems.innerHTML = "";
 
-for ( i=0; i< cart.length; i++){
+  for (i = 0; i < cart.length; i++) {
+    //**************places IMG + Title to cart on click*******************
+    let item = cart[i];
+    let selectedItem = document.createElement("div");
+    let img = document.createElement("img");
+    selectedItem.classList.add("cartImg");
+    selectedItem.setAttribute("id", addItemId);
+    img.setAttribute("src", item.children[0].currentSrc);
 
-    //places items to cart on click
-let item = cart[i];
-    let selectedItem = document.createElement('div');
-    let img = document.createElement('img');
-    selectedItem.classList.add('cartImg');
-    selectedItem.setAttribute('id', addItemId)
-    img.setAttribute('src',item.children[0].currentSrc)
-
-    //adds menu item title to cart
-    let title = document.createElement('div');
+    //**************adds menu item title to cart********************
+    let title = document.createElement("div");
     title.innerText = item.children[1].innerText;
 
-    //adds menu item quantity to cart
-    let label = document.createElement('div');
-    let select = document.createElement('span');
+    //**************adds menu item Instructions to cart**************
+    let label = document.createElement("div");
+    let select = document.createElement("span");
     label.innerText = item.children[2].children[0].innerText;
     select.innerText = item.children[2].children[1].value;
 
-    //adds delete button to cart
-    let delBtn = document.createElement('Button');
-    delBtn.innerText = 'Delete';
-    delBtn.setAttribute('onClick','del('+addItemId+')');
+    //***************adds delete button to cart**********************
+    let delBtn = document.createElement("button");
+    delBtn.innerText = "Delete";
+    delBtn.addEventListener("click", () => {
+      deleteItem(id);
+    });
 
     label.append(select);
     selectedItem.append(img);
@@ -46,23 +46,34 @@ let item = cart[i];
     selectedItem.append(label);
     selectedItem.append(delBtn);
     cartItems.append(selectedItem);
-    }
-}
+  } //end for loop
+} //end cart
 
-function disPrice (){
-    let subtotal = 0;
-    for(i=0; i<cart.length; i++){
-        subtotal += (+cart[i].getAttribute("name"));
-    }
-    cartMath.innerHTML = "";
-    let h1 = document.createElement('h1');
-    h1.innerText = `Subtotal: $ ${subtotal}`
-    cartMath.append(h1);
-    console.log(subtotal);
+//**********************Delete Function**********************
+const deleteItem = (index) => {
+  cart.splice(index, 1);
+  disCart(cart);
+  disPrice(cart);
+};
 
-}
+function disPrice() {
+  let total = 0;
+  for (i = 0; i < cart.length; i++) {
+    total += +cart[i].getAttribute("name");
+  }
 
-//find index of item and splice to delete
-function del(item){
-    document.getElementById(item).remove();
+  //**********************SUBTOTALS**************************
+  let tax = total * 0.047;
+  let subtotal = total + tax;
+  cartMath.innerHTML = "";
+  let h1 = document.createElement("h1");
+  let h2 = document.createElement("h1");
+  let h3 = document.createElement("h1");
+  h1.innerText = `Total: $ ${total.toFixed(2)}`;
+  h2.innerText = `Tax: $ ${tax.toFixed(2)}`;
+  h3.innerText = `Subtotal: $ ${subtotal.toFixed(2)}`;
+  cartMath.append(h1);
+  cartMath.append(h2);
+  cartMath.append(h3);
+  console.log(subtotal.toFixed(2));
 }
